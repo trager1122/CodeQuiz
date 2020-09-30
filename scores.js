0fb42c7570de35f402bb4475650e6c13a7e22206
Thomas Rager <tomrager@yahoo.com>
2020-09-29 23:56:13 -0400

    fixed syntax error on scores.js but sort function nor displaying scores is working

--- scores.js
+++ scores.js
@@ -8,15 +8,19 @@ function displayScores (){
     for (var i=0; i < quizScores.length; i++){
        scoreDisplay.appendChild(quizScores[i]); 
     }
+}
 
 displayScores();
 
+//Click Event for Going back to the Quiz
 var goBack=document.querySelector("#go-back");
 goBack.addEventListener("click", function() {
     window.location.href = "index.html";
 })
 
+//Click Event for Clearing Scores from Local Storage and the page
 var clearScores=document.querySelector("#clear-scores");
 clearScores.addEventListener("click", function() {
     localStorage.clear();
+    location.reload();
 })
